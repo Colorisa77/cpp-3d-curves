34ca0adc4b0c5a1f33954fe49a72329d4e70cd99
Colorisa77 <nikcola777@gmail.com>
2023-10-01 15:18:04 +1000

    operator << and == for structures

--- 3d-curves/common.h
+++ 3d-curves/common.h
@@ -5,21 +5,46 @@
 namespace curve {
     enum class CurveType { CIRCLE, ELLIPSE, HELIX };
 
+    inline std::ostream& operator<<(std::ostream& os, const CurveType type) {
+        switch(type) {
+            case CurveType::CIRCLE:
+                os << "CIRCLE";
+                break;
+            case CurveType::ELLIPSE:
+                os << "ELLIPSE";
+                break;
+            case CurveType::HELIX:
+                os << "HELIX";
+                break;
+        }
+        return os;
+    }
+
     struct Point3D {
         Point3D(double x, double y, double z) : x(x), y(y), z(z) {}
         double x, y, z;
 
-        std::ostream& operator<<(std::ostream& os) const {
-            os << "{" << x << ", " << y << ", " << z << "}";
+        bool operator==(const Point3D& other) const {
+            return x == other.x && y == other.y && z == other.z;
         }
     };
+
+    inline std::ostream& operator<<(std::ostream& os, const Point3D& point) {
+        os << "{" << point.x << ", " << point.y << ", " << point.z << "}";
+        return os;
+    }
 
     struct Vector3D {
         Vector3D(double x, double y, double z) : x(x), y(y), z(z) {}
         double x, y, z;
 
-        std::ostream& operator<<(std::ostream& os) const {
-            os << "{" << x << ", " << y << ", " << z << "}";
+        bool operator==(const Vector3D& other) const {
+            return x == other.x && y == other.y && z == other.z;
         }
     };
+
+    inline std::ostream& operator<<(std::ostream& os, const Vector3D& vector) {
+        os << "{" << vector.x << ", " << vector.y << ", " << vector.z << "}";
+        return os;
+    }
 } // namespace curve
