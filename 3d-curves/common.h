53c201ba81cc96253375c9cd4947740230a9f8d4
Colorisa77 <nikcola777@gmail.com>
2023-10-01 10:58:30 +1000

    adding Point3D and Vector3D structures for data storage

---
+++ 3d-curves/common.h
@@ -0,0 +1,12 @@
+
+namespace curve {
+    struct Point3D {
+        Point3D(double x, double y, double z) : x(x), y(y), z(z) {}
+        double x, y, z;
+    };
+
+    struct Vector3D {
+        Vector3D(double x, double y, double z) : x(x), y(y), z(z) {}
+        double x, y, z;
+    };
+} // namespace curve
