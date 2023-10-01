e4871fe8973fc16ac49adaa6fcdb5a4505a5cb8c
Colorisa77 <nikcola777@gmail.com>
2023-10-01 11:58:57 +1000

    implementation of Ellipse class methods

---
+++ 3d-curves/ellipse.cpp
@@ -0,0 +1,53 @@
+#include "ellipse.h"
+
+#include <cmath>
+
+namespace ellipse {
+    Ellipse::Ellipse(double axis_x, double axis_y)
+    : axis_x_(axis_x)
+    , axis_y_(axis_y){
+    }
+
+    curve::Point3D Ellipse::GetPoint(double t) const {
+        return {GetX(t), GetY(t), GetZ(t)};
+    }
+
+    curve::Vector3D Ellipse::GetDerivative(double t) const {
+        return {GetDerivativeX(t), GetDerivativeY(t), GetDerivativeZ(t)};
+    }
+
+    double Ellipse::GetAxisX() const {
+        return axis_x_;
+    }
+
+    double Ellipse::GetAxisY() const {
+        return axis_y_;
+    }
+
+    double Ellipse::GetX(double t) const {
+        return axis_x_ * std::cos(t);
+    }
+
+    double Ellipse::GetY(double t) const {
+        return axis_y_ * std::sin(t);
+    }
+
+    double Ellipse::GetZ(double t) const {
+        (void)t;
+        return 0.0;
+    }
+
+    double Ellipse::GetDerivativeX(double t) const {
+        return axis_x_ * (-std::sin(t));
+    }
+
+    double Ellipse::GetDerivativeY(double t) const {
+        return axis_y_ * std::cos(t);
+    }
+
+    double Ellipse::GetDerivativeZ(double t) const {
+        (void)t;
+        return 0.0;
+    }
+
+} // namespace ellipse
