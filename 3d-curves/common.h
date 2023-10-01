fe3714d907b2b4641218fd5225d2f680a674efeb
Colorisa77 <nikcola777@gmail.com>
2023-10-01 13:51:51 +1000

    overloading of << operators for structures

--- 3d-curves/common.h
+++ 3d-curves/common.h
@@ -1,3 +1,6 @@
+#pragma once
+
+#include <ostream>
 
 namespace curve {
     enum class CurveType { CIRCLE, ELLIPSE, HELIX };
@@ -5,10 +8,18 @@ namespace curve {
     struct Point3D {
         Point3D(double x, double y, double z) : x(x), y(y), z(z) {}
         double x, y, z;
+
+        std::ostream& operator<<(std::ostream& os) const {
+            os << "{" << x << ", " << y << ", " << z << "}";
+        }
     };
 
     struct Vector3D {
         Vector3D(double x, double y, double z) : x(x), y(y), z(z) {}
         double x, y, z;
+
+        std::ostream& operator<<(std::ostream& os) const {
+            os << "{" << x << ", " << y << ", " << z << "}";
+        }
     };
 } // namespace curve
